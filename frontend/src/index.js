f7af7e450c55d90006c26428d1398e608c24589f
jwookie2 <jwookie2@hotmail.com>
2025-06-28 05:02:14 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+const logoRoutes = require('./routes/logoRoutes');
+app.use('/api/logo', logoRoutes);
 ReactDOM.render(
   <React.StrictMode>
     <App />
